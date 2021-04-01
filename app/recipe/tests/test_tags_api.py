b4d245d48c678752c6a51f965f6e1ca29bcde11c
Lovepreet Singh <singhlovepreet.fzk@gmail.com>
2021-04-01 11:59:21 +0530

    added image field to recipe

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -64,7 +64,7 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
     
-        def test_create_tag_successful(self):
+    def test_create_tag_successful(self):
         """Test creating a new tag"""
         payload = {'name': 'Simple'}
         self.client.post(TAGS_URL, payload)
@@ -81,3 +81,39 @@ class PrivateTagsApiTests(TestCase):
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+	def test_partial_update_recipe(self):
+		"""Test updating a recipe with patch"""
+		recipe = sample_recipe(user=self.user)
+		recipe.tags.add(sample_tag(user=self.user))
+		new_tag = sample_tag(user=self.user, name='Curry')
+	
+		payload = {'title': 'Chicken tikka', 'tags': [new_tag.id]}
+		url = detail_url(recipe.id)
+		self.client.patch(url, payload)
+	
+		recipe.refresh_from_db()
+		self.assertEqual(recipe.title, payload['title'])
+		tags = recipe.tags.all()
+		self.assertEqual(len(tags), 1)
+		self.assertIn(new_tag, tags)
+	
+	def test_full_update_recipe(self):
+		"""Test updating a recipe with put"""
+		recipe = sample_recipe(user=self.user)
+		recipe.tags.add(sample_tag(user=self.user))
+	
+		payload = {
+				'title': 'Spaghetti carbonara',
+				'time_minutes': 25,
+				'price': 5.00
+			}
+		url = detail_url(recipe.id)
+		self.client.put(url, payload)
+	
+		recipe.refresh_from_db()
+		self.assertEqual(recipe.title, payload['title'])
+		self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+		self.assertEqual(recipe.price, payload['price'])
+		tags = recipe.tags.all()
+		self.assertEqual(len(tags), 0)
